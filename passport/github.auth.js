cd7ae31acd501813b1cc0d93ea68d1ec4193c10c
Abhishek Kumar <sahniak56@gmail.com>
2024-03-08 22:34:43 +0530

    add render

--- passport/github.auth.js
+++ passport/github.auth.js
@@ -23,7 +23,7 @@ passport.use(
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL:
-        "https://github-passport-backend.vercel.app/api/auth/github/callback",
+        "https://github-passport-backend.onrender.com/api/auth/github/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       const user = await User.findOne({ username: profile.username });
